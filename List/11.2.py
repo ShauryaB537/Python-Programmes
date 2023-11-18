e86916c60b4e6b2b17887b8e278e112ddc439fb8
Shaurya Bansal <shauryabansal537@gmail.com>
2023-11-18 19:52:10 +0530

    11.2

---
+++ List/11.2.py
@@ -0,0 +1,22 @@
+list_ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+list1 = list_[5:15:2]
+list2=list_[::4]
+sum=avg=0
+
+print('SLICE 1')
+for a in list1:
+    sum+=a
+    print(a, end=" ")
+
+print()
+
+print("Sum of elements of slice 1:", sum)
+print("SLICE 2")
+sum=0
+for a in list2:
+    sum+=a
+    print(a, end=" ")
+
+print()
+avg=sum/len(list2)
+print("Average of elements of slice 2:", avg)
